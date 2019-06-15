3dfd9d31261a8cb3f38a49fdc065575d0d246f94
John Klimis <jpatterson704@yahoo.com>
2019-06-15 15:18:10 +0300

    separated code into files

--- article.php
+++ article.php
@@ -1,36 +1,33 @@
 <?php
+  require 'database.php';
 
-$db_host = "localhost";
-$db_name = "cms";
-$db_user = "cms_www";
-$db_pass = "64w6H2rOJ1zwLRyk";
+if( isset($_GET['id']) &&  is_numeric($_GET['id'])){
+    $sql = "SELECT *
+    FROM article
+    WHERE id = " . $_GET['id'];
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    $results = mysqli_query($conn, $sql);
 
-if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
-    exit;
+    if ( $results === false) {
+        echo mysqli_error($conn);
+    } else {
+        $article = mysqli_fetch_assoc($results);
+    }
+}else{
+    $article = null;
 }
 
-$sql = "SELECT *
-        FROM article
-        WHERE id = 1";
 
-$results = mysqli_query($conn, $sql);
-
-if ($results === false) {
-    echo mysqli_error($conn);
-} else {
-    $article = mysqli_fetch_assoc($results);
-}
 
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>My blog</title>
     <meta charset="utf-8">
 </head>
+
 <body>
 
     <header>
@@ -39,15 +36,16 @@ if ($results === false) {
 
     <main>
         <?php if ($article === null): ?>
-            <p>Article not found.</p>
+        <p>Article not found.</p>
         <?php else: ?>
 
-            <article>
-                <h2><?= $article['title']; ?></h2>
-                <p><?= $article['content']; ?></p>
-            </article>
+        <article>
+            <h2><?= $article['title']; ?></h2>
+            <p><?= $article['content']; ?></p>
+        </article>
 
         <?php endif; ?>
     </main>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,59 +1,37 @@
 <?php
 
-$db_host = "localhost";
-$db_name = "cms";
-$db_user = "cms_www";
-$db_pass = "64w6H2rOJ1zwLRyk";
+    require 'database.php';
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    $sql = "SELECT *
+            FROM article
+            ORDER BY published_at;";
 
-if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
-    exit;
-}
+    $results = mysqli_query($conn, $sql);
 
-$sql = "SELECT *
-        FROM article
-        ORDER BY published_at;";
-
-$results = mysqli_query($conn, $sql);
-
-if ($results === false) {
-    echo mysqli_error($conn);
-} else {
-    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
-}
+    if ($results === false) {
+        echo mysqli_error($conn);
+    } else {
+        $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+    }
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My blog</title>
-    <meta charset="utf-8">
-</head>
-<body>
+<?php require 'header.php'; ?>
 
-    <header>
-        <h1>My blog</h1>
-    </header>
+    <?php if (empty($articles)): ?>
+    <p>No articles found.</p>
+    <?php else: ?>
 
-    <main>
-        <?php if (empty($articles)): ?>
-            <p>No articles found.</p>
-        <?php else: ?>
+    <ul>
+        <?php foreach ($articles as $article): ?>
+        <li>
+            <article>
+                <h2> <a href="article.php?id=<?=$article['id']; ?>"> <?= $article['title']; ?></a></h2>
+                <p><?= $article['content']; ?></p>
+            </article>
+        </li>
+        <?php endforeach; ?>
+    </ul>
 
-            <ul>
-                <?php foreach ($articles as $article): ?>
-                    <li>
-                        <article>
-                            <h2><?= $article['title']; ?></h2>
-                            <p><?= $article['content']; ?></p>
-                        </article>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+    <?php endif; ?>
 
-        <?php endif; ?>
-    </main>
-</body>
-</html>
+<?php require 'footer.php'; ?>
